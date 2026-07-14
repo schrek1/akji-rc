8033ea90caf27111bdaee802b772a259f8fbd1bb
Ondrej Schrek <ondrej.schrek@gmail.com>
2026-07-14 17:47:34 +0200

    P1T2: (capture) restructure capture command flow

--- cmd/akji-capture/main.go
+++ cmd/akji-capture/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"time"
@@ -10,17 +11,56 @@ import (
 	"github.com/schrek1/akji-rc/internal/capture"
 )
 
+const dateLayout = "2006-01-02 15:04:05"
+
+var defaultLogger = log.New(os.Stdout, time.Now().Format(dateLayout)+" - ", 0)
+
 func main() {
-	logger := log.New(os.Stdout, time.Now().Format("2006-01-02 15:04:05")+" - ", 0)
-	if err := run(os.Args[1:], logger); err != nil {
-		fmt.Fprintf(os.Stderr, "%s - ERROR: %v\n", time.Now().Format("2006-01-02 15:04:05"), err)
+	if err := run(os.Args[1:], defaultLogger); err != nil {
+		fmt.Fprintf(os.Stderr, "%s - ERROR: %v\n", time.Now().Format(dateLayout), err)
 		os.Exit(1)
 	}
 }
 
 func run(args []string, logger *log.Logger) error {
+	scriptOptions, err := parseScriptOptions(args, os.Stdout)
+	if err != nil {
+		return err
+	}
+
+	workDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	config, err := capture.LoadConfig(capture.Environment(), workDir)
+	if err != nil {
+		return err
+	}
+
+	if scriptOptions.isSingleShot() {
+		return runSingleCapture(config, scriptOptions.outputPath, workDir, logger)
+	}
+
+	return runLoop(config, scriptOptions.timeLapse, workDir, logger)
+}
+
+func runSingleCapture(config capture.CapturingConfiguration, outputPath string, workDir string, logger *log.Logger) error {
+	if outputPath == "" {
+		outputPath = capture.DefaultOutputPath(workDir, time.Now())
+	}
+
+	if err := capture.CaptureToFile(config, outputPath, logger); err != nil {
+		return err
+	}
+
+	logger.Println("Single capture successful.")
+	return nil
+}
+
+func parseScriptOptions(args []string, output io.Writer) (ScriptOptions, error) {
 	flags := flag.NewFlagSet("akji-capture", flag.ContinueOnError)
-	flags.SetOutput(os.Stdout)
+	flags.SetOutput(output)
 
 	var outputPath string
 	var timeLapse int
@@ -39,38 +79,20 @@ func run(args []string, logger *log.Logger) error {
 
 	if err := flags.Parse(args); err != nil {
 		if err == flag.ErrHelp {
-			return nil
+			return ScriptOptions{}, nil
 		}
-		return err
+		return ScriptOptions{}, err
 	}
 	if timeLapse > 0 && outputPath != "" {
-		return fmt.Errorf("--out is not compatible with --timeLapse")
+		return ScriptOptions{}, fmt.Errorf("--out is not compatible with --timeLapse")
 	}
-
-	workDir, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	config, err := capture.LoadConfig(capture.Environment(), workDir)
-	if err != nil {
-		return err
-	}
-
-	if timeLapse > 0 {
-		return runLoop(config, time.Duration(timeLapse)*time.Second, workDir, logger)
-	}
-
-	if outputPath == "" {
-		outputPath = capture.DefaultOutputPath(workDir, time.Now())
-	}
-	if err := capture.CaptureToFile(config, outputPath, logger); err != nil {
-		return err
-	}
-	logger.Println("Single capture successful.")
-	return nil
+	return ScriptOptions{
+		outputPath: outputPath,
+		timeLapse:  time.Duration(timeLapse) * time.Second,
+	}, nil
 }
 
-func runLoop(config capture.Config, interval time.Duration, workDir string, logger *log.Logger) error {
+func runLoop(config capture.CapturingConfiguration, interval time.Duration, workDir string, logger *log.Logger) error {
 	logger.Printf("Time-lapse enabled (Interval: %.0fs). Press Ctrl+C to stop.", interval.Seconds())
 	nextTick := time.Now()
 	for {
@@ -90,4 +112,13 @@ func runLoop(config capture.Config, interval time.Duration, workDir string, logg
 		logger.Printf("Waiting %.0fs until next capture...", sleepDuration.Seconds())
 		time.Sleep(sleepDuration)
 	}
+}
+
+type ScriptOptions struct {
+	outputPath string
+	timeLapse  time.Duration
+}
+
+func (o ScriptOptions) isSingleShot() bool {
+	return o.timeLapse <= 0
 }
